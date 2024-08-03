6df9a806b0e39396574a84faa4bc41c3c04d8854
k sandeep <sandeepsandy65897@gmail.com>
2024-08-03 15:37:11 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-function App() {
+import AdminLayout from './layouts/admin/AdminLayout';
+
+import {} from 'react-router-dom';
+
+
+import {
+  ChakraProvider,
+  // extendTheme
+} from '@chakra-ui/react';
+import initialTheme from './theme/theme'; //  { themeGreen }
+import { useState } from 'react';
+
+
+export default function Main() {
+
+  const [currentTheme, setCurrentTheme] = useState(initialTheme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={currentTheme}>
+      <Routes>
+        <Route path="auth/*" element={<Navigate to="/admin" replace />} />
+        <Route
+          path="admin/*"
+          element={
+            <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
+          }
+        />
+        <Route path="rtl/*" element={<Navigate to="/admin" replace />} />
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+      </Routes>
+    </ChakraProvider>
   );
 }
-
-export default App;
